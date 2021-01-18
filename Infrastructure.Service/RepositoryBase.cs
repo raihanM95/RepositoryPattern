89bdb8e35f40b08aa6bc8d96212ed02e10037a62
raihanM95 <mahmud.raihan@yahoo.com>
2021-01-18 14:35:08 +0600

    RepositoryBase implemented

---
+++ Infrastructure.Service/RepositoryBase.cs
@@ -0,0 +1,40 @@
+﻿using Application;
+using Infrastructure.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Infrastructure.Service
+{
+    public abstract class RepositoryBase<T> : IRepositoryBase<T> where T : class
+    {
+        protected RepositoryContext RepositoryContext { get; set; }
+        public RepositoryBase(RepositoryContext repositoryContext)
+        {
+            this.RepositoryContext = repositoryContext;
+        }
+        public IQueryable<T> FindAll()
+        {
+            return this.RepositoryContext.Set<T>().AsNoTracking();
+        }
+        public IQueryable<T> FindByCondition(Expression<Func<T, bool>> expression)
+        {
+            return this.RepositoryContext.Set<T>().Where(expression).AsNoTracking();
+        }
+        public void Create(T entity)
+        {
+            this.RepositoryContext.Set<T>().Add(entity);
+        }
+        public void Update(T entity)
+        {
+            this.RepositoryContext.Set<T>().Update(entity);
+        }
+        public void Delete(T entity)
+        {
+            this.RepositoryContext.Set<T>().Remove(entity);
+        }
+    }
+}
