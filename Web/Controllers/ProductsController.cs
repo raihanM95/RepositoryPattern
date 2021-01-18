32d8d1b098b70f2d808b97fe05c7f4a12bbe8c5d
raihanM95 <mahmud.raihan@yahoo.com>
2021-01-18 17:21:47 +0600

    CRUD for example

---
+++ Web/Controllers/ProductsController.cs
@@ -0,0 +1,107 @@
+﻿using Application;
+using Domain.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Web.Models;
+
+namespace Web.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private IRepositoryWrapper _repoWrapper;
+        public ProductsController(IRepositoryWrapper repoWrapper)
+        {
+            _repoWrapper = repoWrapper;
+        }
+
+        // GET: ProductsController
+        public IActionResult Index()
+        {
+            var products = _repoWrapper.Product.FindAll();
+            return View(products);
+        }
+
+        // GET: ProductsController/Details/5
+        public ActionResult Details(int id)
+        {
+            Product product = _repoWrapper.Product.FindByCondition(i => i.Id == id).FirstOrDefault();
+            return View(product);
+        }
+
+        // GET: ProductsController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ProductsController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Product product)
+        {
+            try
+            {
+                _repoWrapper.Product.Create(product);
+                _repoWrapper.Save();
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProductsController/Edit/5
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Product product = _repoWrapper.Product.FindByCondition(i => i.Id == id).FirstOrDefault();
+            return View(product);
+        }
+
+        // POST: ProductsController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Product product)
+        {
+            try
+            {
+                _repoWrapper.Product.Update(product);
+                _repoWrapper.Save();
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProductsController/Delete/5
+        public IActionResult Delete(int id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            try
+            {
+                Product product = _repoWrapper.Product.FindByCondition(i => i.Id == id).FirstOrDefault();
+                _repoWrapper.Product.Delete(product);
+                _repoWrapper.Save();
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Web/Models/ProductViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web.Models
+{
+    public class ProductViewModel
+    {
+        //public virtual int Id { get; set; }
+        //public string Name { get; set; }
+        //public string Barcode { get; set; }
+        //public string Description { get; set; }
+        //public string CreatedBy { get; set; }
+        //public DateTime Created { get; set; }
+        //public string LastModifiedBy { get; set; }
+        //public DateTime? LastModified { get; set; }
+    }
+}
