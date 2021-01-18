c353846d4d6963083b4639da2058c2ebf69d607d
raihanM95 <mahmud.raihan@yahoo.com>
2021-01-18 14:49:09 +0600

    ProductRepository implemented

---
+++ Application/Interfaces/IProductRepository.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Interfaces
+{
+    public interface IProductRepository : IRepositoryBase<Product>
+    {
+    }
+}
---
+++ Infrastructure.Service/Repositories/ProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using Application.Interfaces;
+using Domain.Entities;
+using Infrastructure.Persistence.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Service.Repositories
+{
+    public class ProductRepository : RepositoryBase<Product>, IProductRepository
+    {
+        public ProductRepository(RepositoryContext repositoryContext)
+            : base(repositoryContext)
+        {
+        }
+    }
+}
