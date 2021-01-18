4567015ac8cd2b66ae66b64ed1b62f9235ca72de
raihanM95 <mahmud.raihan@yahoo.com>
2021-01-18 11:20:03 +0600

    Create Product Entities

---
+++ Domain/Entities/Product.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Entities
+{
+    public class Product : AuditableBaseEntity
+    {
+        public string Name { get; set; }
+        public string Barcode { get; set; }
+        public string Description { get; set; }
+    }
+}
