535bd6c70765566f86d9068ade2932efebda345d
raihanM95 <mahmud.raihan@yahoo.com>
2021-01-18 11:38:44 +0600

    Create RepositoryContext and added Product

---
+++ Infrastructure.Persistence/Contexts/RepositoryContext.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Persistence.Contexts
+{
+    public class RepositoryContext : DbContext
+    {
+        public RepositoryContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
